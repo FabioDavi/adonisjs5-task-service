99c00984219494c863e63bd97ac02b698e8d99b3
Fabio Davi <fdr.desenvolvedor@gmail.com>
2020-07-31 21:56:16 -0300

    dando uma versatilidade no codigo

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -1,13 +1,29 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Tasks from 'App/Models/Task'
+import { rules, schema } from '@ioc:Adonis/Core/Validator'
 
 export default class TasksController {
   public index ({view}:HttpContextContract){
     return view.render('tasks/index')
   }
   public async store ({request, response}:HttpContextContract){
+    const validationSchema = schema.create({
+      title: schema.string({ trim: true }, [
+        rules.maxLength(255),
+      ]),
+    })
+
+    const validatedData = await request.validate({
+      schema: validationSchema,
+      messages: {
+        'title.required': 'Enter task title',
+        'title.maxLength': 'Task title can not exceed 255 character',
+      },
+    })
+
     await Tasks.create({
-      title: request.input('title'),
+      //title: request.input('title'),
+      title: validatedData.title,
     })
     return response.redirect('back')
   }
